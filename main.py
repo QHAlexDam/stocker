7cef3940a117ef5a7d501263d95354ce554561a1
alex <dam>
2021-04-02 02:35:01 -0400

    refactor

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
 from imports import *
 load_dotenv(find_dotenv())
 
+#INPUT DATA
+STOCK_TICKER = "AAL"
+FILE_NAME = 'stock_market_data-%s.csv'%ticker
+#values depend on sample size
+WINDOW_SIZE = 400 
+RANGE_INIT = 0
+RANGE_END = 1600
+
 #Getting and storing data in a cvs file
-ticker = "AAL"
-file_to_save = 'stock_market_data-%s.csv'%ticker
-df = stockToCSV(file_to_save, ticker)
+df = stockToCSV(FILE_NAME, STOCK_TICKER)
 
-#Graphing data
-#plt.figure(figsize = (19,9))
-#plt.plot(range(df.shape[0]),(df['3. low']+df['2. high'])/2.0)
-#plt.xticks(range(0,df.shape[0],500),df['date'].loc[::500],rotation=45)
-#plt.xlabel('date',fontsize=18)
-#plt.ylabel('Mid Price',fontsize=18)
-#plt.show()
-
-winSize = 400
-rangei=0
-rangef=1600
-
-scaler = createDataSets(df, winSize, rangei, rangef)[0]
-trainingData = createDataSets(df, winSize, rangei, rangef)[1]
-testingData = createDataSets(df, winSize, rangei, rangef)[2]
-all_mid_data = createDataSets(df, winSize, rangei, rangef)[3]
-
+#creating data sets
+scaler, trainingData, testingData, all_mid_data  = createDataSets(df, WINDOW_SIZE, RANGE_INIT, RANGE_END)
 
 #standard average
 std_avg_prediction(df, trainingData, all_mid_data)
